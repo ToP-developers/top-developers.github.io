eb2702edd7e8fca190fba1434e3813d96ebcd78e
Svoykina N <nadya.svojkina@gmail.com>
2018-04-09 18:08:53 +0300

    Deleted unused packages

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const config = {
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'index.js'
     },
     module: {
         rules: [
@@ -25,11 +25,21 @@ const config = {
             {
                 test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
                 loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'static/fonts/',
+                    publicPath: 'static/fonts/'
+                }
             },
             {
                 test: /\.(png|jpg|jpeg|gid|svg)$/,
                 use: {
-                    loader: 'file-loader'
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'static/img/',
+                        publicPath: 'static/img/'
+                    }
                 }
             }
 
